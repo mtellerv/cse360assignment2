034dd9a9d879e3c304fc3889c354ad38428b7b41
Marc Tellerva <mtellerv@asu.edu>
2020-10-05 20:46:02 -0700

    Documentation of AddingMachine.

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,27 +1,52 @@
 package cse360assignment02;
 
+/**
+ * Tracks arithmetic operations performed on a single held value.
+ */
 public class AddingMachine {
 
     private int total;
 
+    /**
+     * Creates a new AddingMachine with a default value of 0 and an empty history.
+     */
     public AddingMachine() {
         total = 0;  // not needed - included for clarity
     }
 
+    /**
+     * Returns the current value held by this AddingMachine.
+     */
     public int getTotal() {
         return 0;
     }
 
+    /**
+     * Performs addition on the value held by this AddingMachine.
+     *
+     * @param value the value to add
+     */
     public void add(int value) {
     }
 
+    /**
+     * Performs subtraction on the value held by this AddingMachine.
+     *
+     * @param value the value to subtract
+     */
     public void subtract(int value) {
     }
 
+    /**
+     * Returns a string representation of the operations performed on this AddingMachine.
+     */
     public String toString() {
         return "";
     }
 
+    /**
+     * Clears the history of this AddingMachine and resets its value to 0.
+     */
     public void clear() {
     }
 
